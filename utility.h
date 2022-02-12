ed329aa75d0f14c3e3550a2395b463a2f0013659
yash-patel268 <yashp2126@gmail.com>
2022-02-12 17:11:25 -0500

    Added more commands

--- utility.h
+++ utility.h
@@ -5,16 +5,22 @@
 
 extern int strTokenizer(char* str, char tokens[][BUFFER_LEN]);
 
-extern void getCurrentDirectory(char* pwd);
-
 extern void changeDirectory(char* pwd, char* path);
 
 extern void clearScreen(void);
 
-extern void pauseShell(void);
+extern void getCurrentDirectory(char* pwd);
 
 extern void displayDirectory(char* path);
 
 extern void displayEnviron(char env[][BUFFER_LEN]);
+
+extern void echoShell(int argc, char *argc[]);
+
+extern void helpShell(void);
+
+extern void pauseShell(void);
+
+extern void quitShell(void);
 
 #endif /* UTILITY_H_ */
