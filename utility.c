359524192d126eedaf6b1df9a6f022c0a240d339
yash-patel268 <yashp2126@gmail.com>
2022-02-13 15:43:04 -0500

    Fixed formatting again

--- utility.c
+++ utility.c
@@ -7,17 +7,17 @@
 #include "utility.h" 
 
 int strTokenizer(char* str, char tokens[][BUFFER_LEN]){
-char delimiter[2] = " ";
-int tokens_count = 0;
-char* token = strtok(str, delimiter);
+	char delimiter[2] = " ";
+	int tokens_count = 0;
+	char* token = strtok(str, delimiter);
 
-while (token != NULL){
-	strcpy(tokens[tokens_count], token);
-	tokens_count++;
-	token = strtok(NULL, delimiter);
-}
+	while (token != NULL){
+		strcpy(tokens[tokens_count], token);
+		tokens_count++;
+		token = strtok(NULL, delimiter);
+	}
 
-return tokens_count;
+	return tokens_count;
 }
 
 void getCurrentDirectory(char* pwd){
@@ -41,20 +41,20 @@ void clearScreen(void){
 }
 
 void displayDirectory(char* path){
-DIR *dirptr;
-struct dirent *item;
+	DIR *dirptr;
+	struct dirent *item;
 
-dirptr = opendir(".");
+	dirptr = opendir(".");
 
-if (dirptr == NULL){
-	return 1;
-}
+	if (dirptr == NULL){
+		return 1;
+	}
 
-while(( item = readdir(dirptr) )){
-	printf("%s\n", item->d_name);
-}
-printf("\n");
-closedir(dirptr);
+	while(( item = readdir(dirptr) )){
+		printf("%s\n", item->d_name);
+	}
+	printf("\n");
+	closedir(dirptr);
 }
 
 void displayEnviron(char env[][BUFFER_LEN]){
@@ -69,26 +69,26 @@ void echoShell(char* pwd, int token_count, char tokens[][BUFFER_LEN]){
 }
 
 void helpShell(void){
-printf("\nList of available commands:\n\n");
-printf("help\t\t\t Lists the help menu.\n");
-printf("dir <directory>\t\t Lists the contents of the directory\n");
-printf("environ\t\t\t Lists all the environment strings..\n");
-printf("echo <comment>\t\t Displays comment on the command line followed by a newline.\n");
-printf("cd <path>\t\t Changes the working directory to the specified path.\n");
-printf("clr\t\t\t Clears the screen.\n");
-printf("pause\t\t\t Pauses operation of the shell until the ENTER key is hit.\n");
-printf("quit\t\t\t Quits the shell.\n\n\n");
+	printf("\nList of available commands:\n\n");
+	printf("help\t\t\t Lists the help menu.\n");
+	printf("dir <directory>\t\t Lists the contents of the directory\n");
+	printf("environ\t\t\t Lists all the environment strings..\n");
+	printf("echo <comment>\t\t Displays comment on the command line followed by a newline.\n");
+	printf("cd <path>\t\t Changes the working directory to the specified path.\n");
+	printf("clr\t\t\t Clears the screen.\n");
+	printf("pause\t\t\t Pauses operation of the shell until the ENTER key is hit.\n");
+	printf("quit\t\t\t Quits the shell.\n\n\n");
 }
 
 void pauseShell(void){
-printf("\n\nShell is now paused. Please press ENTER key to continue....\n\n");
-char code = (char)getchar();
-while (code != '\n'){
-	code = (char)getchar();
-}
+	printf("\n\nShell is now paused. Please press ENTER key to continue....\n\n");
+	char code = (char)getchar();
+	while (code != '\n'){
+		code = (char)getchar();
+	}
 }
 
 void quitShell(void){
-printf("Closing....\n");
-return EXIT_SUCCESS;
+	printf("Closing....\n");
+	return EXIT_SUCCESS;
 }
